0adae5451034aaef3ef9b85ed26738d3bfd0a3e3
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-09-20 09:19:17 +0600

    toggle added

---
+++ features.js
@@ -0,0 +1,10 @@
+document.getElementById('addMoneybtn').addEventListener('click',function(){
+    showSectionById('AddMoneyForm');
+
+})
+
+
+document.getElementById('cashOutbtn').addEventListener('click',function(){
+    showSectionById('cashOutForm');
+
+})
--- utilities.js
+++ utilities.js
@@ -1,14 +1,27 @@
 // common function 
 
-function getInputFieldValueById(id){
+function getInputFieldValueById(id) {
     const inputValue = document.getElementById(id).value;
     const inputValueNum = parseFloat(inputValue);
     return inputValueNum;
 }
 
 
-function getTextFieldValueById(id){
+function getTextFieldValueById(id) {
     const textValue = document.getElementById(id).innerText;
     const textValueNum = parseFloat(textValue);
     return textValueNum;
 }
+
+
+function showSectionById(id) {
+    const addForm = document.getElementById('AddMoneyForm').classList.add('hidden');
+    
+
+    document.getElementById('cashOutForm').classList.add('hidden');
+
+    // document.getElementById('transactionList').classList.add('hidden');
+    
+    document.getElementById(id).classList.remove('hidden');
+}
+
