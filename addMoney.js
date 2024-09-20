7a3261f94ee29e3996b43e9c20066429a2b87175
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-09-20 10:39:01 +0600

    transaction added

--- addMoney.js
+++ addMoney.js
@@ -4,12 +4,23 @@ document.getElementById('btn-add-money').addEventListener('click', function (e) 
     const addMoney = getInputFieldValueById('inputAddMoney');
     const inputPin = getInputFieldValueById('inputPin');
 
+
+    if(isNaN(addMoney)){
+        alert('faild to add money');
+        return;
+    }
+
     if (inputPin === 1234) {
 
         const balance = getTextFieldValueById('availableBalance');
         const newBalance = balance + addMoney;
 
         document.getElementById('availableBalance').innerText = newBalance;
+
+        const p = document.createElement('p');
+        p.innerHTML = `Added : ${addMoney} tk. New Balance : ${newBalance} tk.`
+
+        document.getElementById('transationContainer').appendChild(p);
 
     } else {
         alert('failed to add money')
--- cashOut.js
+++ cashOut.js
@@ -3,11 +3,26 @@ document.getElementById('btn-cash-out').addEventListener('click',function(e){
     const cashOut = getInputFieldValueById('inputCashOut');
     const conPin = getInputFieldValueById('confirmPin');
 
+    if(isNaN(cashOut)){
+        alert('faild to cash out');
+        return;
+    }
+
     if(conPin === 1234){
         const balance = getTextFieldValueById('availableBalance');
         if(balance > cashOut){
             const newBalance = balance - cashOut;
             document.getElementById('availableBalance').innerText = newBalance;
+
+            const div = document.createElement('div');
+            div.classList.add('bg-yellow-300');
+            div.innerHTML = `
+                <h4 class="text-2xl font-bold">Cash Out</h4>
+                <p>${cashOut} withdraw. New Balance ${newBalance}</p>
+            `
+
+            document.getElementById('transationContainer').appendChild(div);
+
         }else{
             alert('insufficient balance');
         }
